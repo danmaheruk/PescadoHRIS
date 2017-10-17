94e831cd778e8b35374d0d581c5aac2d6e3bfb76
danmaheruk <dan.maher@me.com>
2017-10-17 11:53:38 +0100

    Finish Layout and Routes

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jbuilder'
  gem 'devise'
  gem 'guard'
  gem 'minitest'
+ gem 'rails-controller-testing'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
 
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+# Soft named routed paths
+
+  get '/help', to: 'static_pages#help'
+  get '/signup', to: 'users#new'
+
+
+  # Static routes
+
+  get '/static_pages/home'
+  get '/static_pages/help'
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
