2a054fc04d5fa0c8d22d0d0db6dcc5d558228977
danmaheruk <dan.maher@me.com>
2017-10-18 12:07:46 +0100

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -36,10 +36,10 @@ gem 'jbuilder'
  gem 'minitest'
  gem 'rails-controller-testing'
  gem 'rb-readline'
+ gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara'
   gem 'selenium-webdriver'
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root 'static_pages#home'
 
 # Soft named routed paths
-
   get '/help', to: 'static_pages#help'
   get '/signup', to: 'users#new'
-
+  post '/signup', to: 'users#create'
 
   # Static routes
-
   get '/static_pages/home'
   get '/static_pages/help'
+  get '/users/new'
+  post '/users/create'
 
-
-
-
+ # Resources
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
